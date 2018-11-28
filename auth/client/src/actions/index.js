c542ff106e5db80a9c9e33e1220e94089546c48d
dtafesse <dtafesse@uwm.edu>
2018-11-28 17:07:15 -0600

    dispatching actions

--- auth/client/src/actions/index.js
+++ auth/client/src/actions/index.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 import { AUTH_USER } from "./types";
 
-export const signup = ({ email, password }) => dispatch => {
-  axios.post("http://localhost:3090/signup", {
+export const signup = ({ email, password }) => async dispatch => {
+  const response = await axios.post("http://localhost:3090/signup", {
     email,
     password
   });
+
+  dispatch({ type: AUTH_USER, payload: response.data.token });
 };
